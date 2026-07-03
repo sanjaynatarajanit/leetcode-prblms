4330ccde571c54b45f8757be398dd76efceca202
sanjaynatarajanit <sanjaynatarajanit@gmail.com>
2026-07-03 10:48:43 +0530

    Time: 0 ms (100%), Space: 42.7 MB (82.38%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,25 +1,26 @@
 import java.util.*;
-
 class Solution {
     public String[] findWords(String[] words) {
-        String[] rows = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+        String r1 = "qwertyuiop";
+        String r2 = "asdfghjkl";
+        String r3 = "zxcvbnm";
+
         List<String> ans = new ArrayList<>();
 
         for (String word : words) {
             String s = word.toLowerCase();
-            String row = rows[rows[0].indexOf(s.charAt(0)) != -1 ? 0 :
-                              rows[1].indexOf(s.charAt(0)) != -1 ? 1 : 2];
 
-            boolean ok = true;
-            for (char c : s.toCharArray())
-                if (row.indexOf(c) == -1) {
-                    ok = false;
-                    break;
-                }
-
-            if (ok) ans.add(word);
+            if (check(s, r1) || check(s, r2) || check(s, r3))
+                ans.add(word);
         }
 
         return ans.toArray(new String[0]);
+    }
+
+    boolean check(String s, String row) {
+        for (char c : s.toCharArray())
+            if (row.indexOf(c) == -1)
+                return false;
+        return true;
     }
 }
