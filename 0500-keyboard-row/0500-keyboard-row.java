e387bac9ce3d69cf35028d93c8419610bbcd982b
sanjaynatarajanit <sanjaynatarajanit@gmail.com>
2026-07-03 10:51:15 +0530

    Refactor findWords method to check keyboard rows

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -24,3 +24,32 @@ class Solution {
         return true;
     }
 }
+
+//or
+
+import java.util.*;
+class Solution {
+    public String[] findWords(String[] words) {
+        String r1 = "qwertyuiop";
+        String r2 = "asdfghjkl";
+        String r3 = "zxcvbnm";
+
+        List<String> ans = new ArrayList<>();
+
+        for (String word : words) {
+            String s = word.toLowerCase();
+
+            if (check(s, r1) || check(s, r2) || check(s, r3))
+                ans.add(word);
+        }
+
+        return ans.toArray(new String[0]);
+    }
+
+    boolean check(String s, String row) {
+        for (char c : s.toCharArray())
+            if (row.indexOf(c) == -1)
+                return false;
+        return true;
+    }
+}
